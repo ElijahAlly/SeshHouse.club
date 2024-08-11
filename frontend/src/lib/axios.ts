cae1c4cfd9108724bcf6fe0ab97393ee50dd6d50
MusaByte <75961076+ElijahAlly@users.noreply.github.com>
2024-08-11 11:08:35 -0400

    updated headers

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -4,6 +4,10 @@ axios.defaults.withCredentials = true;
 
 const instance = axios.create({
     baseURL: 'https://sesh-house-koa-server-production.up.railway.app/api', // prod is https://sesh-house-koa-server-production.up.railway.app/api and dev is http://localhost:3000/api
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+    },
 });
 
 export default instance;
