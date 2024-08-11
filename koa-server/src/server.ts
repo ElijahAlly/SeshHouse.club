48646b6b81bc1ce5a86c8fcce20f6c807e54719a
MusaByte <75961076+ElijahAlly@users.noreply.github.com>
2024-08-11 13:31:57 -0400

    changed to express

--- koa-server/src/server.ts
+++ koa-server/src/server.ts
@@ -1,4 +1,8 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
 import Koa from 'koa';
 import session from 'koa-session';
 import bodyParser from 'koa-bodyparser';
@@ -15,35 +19,48 @@ import fileRouter from './routes/file';
 import swaggerRouter from './routes/swagger';
 import emailRouter from './routes/email';
 
-const app = new Koa();
+dotenv.config();
 
-app.keys = [process.env.SESSION_SECRET || `session-secret-you-WILL-NEVER_guess:)-${(Math.random() * 1000).toFixed(0)}`];
-app.use(bodyParser());
-app.use(cors());
-app.use(logger());
+// const app = new Koa();
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const PORT = CONFIG.port;
-app.use(session(CONFIG.SESSION_CONFIG, app));
+// Middleware
+app.use(bodyParser.json());
+app.use(cors({
+    origin: ['https://www.seshhouse.club', 'https://seshhouse.club'],
+    credentials: true,
+}));
+app.use(express.json());
+// app.keys = [process.env.SESSION_SECRET || `session-secret-you-WILL-NEVER_guess:)-${(Math.random() * 1000).toFixed(0)}`];
+// app.use(bodyParser());
+// app.use(cors());
+// app.use(logger());
 
-app.use(healthcheckRouter.routes());
-app.use(userRouter.routes()).use(userRouter.allowedMethods());
-app.use(eventRouter.routes()).use(eventRouter.allowedMethods());
-app.use(eventToBookRouter.routes()).use(eventToBookRouter.allowedMethods());
-app.use(fileRouter.routes()).use(fileRouter.allowedMethods());
-app.use(emailRouter.routes()).use(emailRouter.allowedMethods());
-app.use(async (ctx) => {
-    ctx.type = 'application/json';
-});
-// TODO: Add blogs route
-app.use(
-    koaSwagger({
-        routePrefix: '/',
-        swaggerOptions: {
-            url: '/swagger.json',
-        },
-    })
-);
-app.use(swaggerRouter.routes()).use(swaggerRouter.allowedMethods());
+// const PORT = CONFIG.port;
+// app.use(session(CONFIG.SESSION_CONFIG, app));
+
+app.use('/api/event_to_book', eventToBookRouter);
+
+// app.use(healthcheckRouter.routes());
+// app.use(userRouter.routes()).use(userRouter.allowedMethods());
+// app.use(eventRouter.routes()).use(eventRouter.allowedMethods());
+// app.use(eventToBookRouter.routes()).use(eventToBookRouter.allowedMethods());
+// app.use(fileRouter.routes()).use(fileRouter.allowedMethods());
+// app.use(emailRouter.routes()).use(emailRouter.allowedMethods());
+// app.use(async (ctx) => {
+//     ctx.type = 'application/json';
+// });
+// // TODO: Add blogs route
+// app.use(
+//     koaSwagger({
+//         routePrefix: '/',
+//         swaggerOptions: {
+//             url: '/swagger.json',
+//         },
+//     })
+// );
+// app.use(swaggerRouter.routes()).use(swaggerRouter.allowedMethods());
 
 const server = app
     .listen(PORT, () => {
