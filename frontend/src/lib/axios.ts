900f1f7d931429002198afca7c5cdb3c59cb6e17
MusaByte <75961076+ElijahAlly@users.noreply.github.com>
2024-08-11 10:48:08 -0400

    removed axios header

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.withCredentials = true;
+axios.defaults.withCredentials = false;
 
 const instance = axios.create({
     baseURL: 'https://sesh-house-koa-server-production.up.railway.app/api', // prod is https://sesh-house-koa-server-production.up.railway.app/api and dev is http://localhost:3000/api
